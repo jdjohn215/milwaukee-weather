56c922dcdc4c9ef220ff1caa075011d217f759ca
jdjohn215 <jdjohn215@gmail.com>
2022-02-19 16:23:24 -0600

    init

---
+++ R/BuildDailyHigh.R
@@ -0,0 +1,187 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+ghcn <- read_csv("data/GHCN_USW00014839.csv")
+
+year.to.plot <- max(ghcn$year)
+last.date <- max(ghcn$date)
+
+this.year <- ghcn %>%
+  filter(year == year.to.plot)
+
+daily.summary.stats <- ghcn %>%
+  filter(year != year.to.plot) %>%
+  select(day_of_year, PRCP, TMAX, TMIN) %>%
+  pivot_longer(cols = -day_of_year) %>%
+  group_by(day_of_year, name) %>%
+  summarise(max = max(value, na.rm = T),
+            min = min(value, na.rm = T),
+            x5 = quantile(value, 0.05, na.rm = T),
+            x20 = quantile(value, 0.2, na.rm = T),
+            x40 = quantile(value, 0.4, na.rm = T),
+            x60 = quantile(value, 0.6, na.rm = T),
+            x80 = quantile(value, 0.8, na.rm = T),
+            x95 = quantile(value, 0.95, na.rm = T)) %>%
+  ungroup()
+
+# month breaks
+month.breaks <- ghcn %>%
+  filter(year == 2019) %>%
+  group_by(month) %>%
+  slice_min(order_by = day_of_year, n = 1) %>%
+  ungroup() %>%
+  select(month, day_of_year) %>%
+  mutate(month_name = month.abb)
+
+record.status.this.year <- this.year %>%
+  select(day_of_year, PRCP, TMAX, TMIN) %>%
+  pivot_longer(cols = -day_of_year, values_to = "this_year") %>%
+  inner_join(daily.summary.stats %>% select(-starts_with("x"))) %>%
+  mutate(record_status = case_when(
+    this_year > max ~ "max",
+    this_year < min ~ "min",
+    TRUE ~ "none"
+  )) %>%
+  filter(record_status != "none")
+
+max.graph <- daily.summary.stats %>%
+  filter(name == "TMAX") %>%
+  ggplot(aes(x = day_of_year)) +
+  # draw vertical lines for the months
+  geom_vline(xintercept = c(month.breaks$day_of_year, 365),
+             linetype = "dotted", lwd = 0.2) +
+  # ribbon between the lowest and 5th percentiles
+  geom_ribbon(aes(ymin = min, ymax = x5),
+              fill = "#bdc9e1") +
+  # ribbon between the 5th and 20th percentiles
+  geom_ribbon(aes(ymin = x5, ymax = x20),
+              fill = "#74a9cf") +
+  # ribbon between the 20th and 40th percentiles
+  geom_ribbon(aes(ymin = x20, ymax = x40),
+              fill = "#2b8cbe") +
+  # ribbon between the 40th and 60th percentiles
+  geom_ribbon(aes(ymin = x40, ymax = x60),
+              fill = "#045a8d") +
+  # ribbon between the 60th and 80th percentiles
+  geom_ribbon(aes(ymin = x60, ymax = x80),
+              fill = "#2b8cbe") +
+  # ribbon between the 80th and 95th percentiles
+  geom_ribbon(aes(ymin = x80, ymax = x95),
+              fill = "#74a9cf") +
+  # ribbon between the 95th percentile and the max
+  geom_ribbon(aes(ymin = x95, ymax = max),
+              fill = "#bdc9e1") +
+  # y-axis breaks
+  geom_hline(yintercept = seq(-10, 100, 10),
+             color = "white", lwd = 0.1) +
+  # line for this year's values
+  geom_line(data = this.year,
+            aes(y = TMAX)) +
+  # points for maximum records set this year
+  geom_point(data = filter(record.status.this.year, 
+                           name == "TMAX",
+                           record_status == "max"),
+             aes(y = this_year), color = "red") +
+  # points for minimum records set this year
+  geom_point(data = filter(record.status.this.year,
+                           name == "TMAX",
+                           record_status == "min"),
+             aes(y = this_year), color = "blue") +
+  scale_y_continuous(breaks = seq(-10, 100, 10),
+                     labels = scales::unit_format(suffix = "°"),
+                     expand = expansion(0.01),
+                     name = NULL,
+                     sec.axis = dup_axis()) +
+  scale_x_continuous(expand = expansion(0),
+                     breaks = month.breaks$day_of_year + 15,
+                     labels = month.breaks$month_name,
+                     name = NULL) +
+  labs(title = "Daily High Temperature at Milwaukee's Mitchell Airport",
+       subtitle = paste("Daily weather summaries from April 1, 1938 through",
+                        format(last.date, "%B %d, %Y"))) +
+  theme(panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        plot.background = element_rect(fill = "linen",
+                                       colour = "linen"),
+        plot.title.position = "plot",
+        plot.title = element_text(face = "bold", size = 16),
+        axis.ticks = element_blank())
+
+
+legend.df <- daily.summary.stats %>%
+  filter(day_of_year %in% 165:201,
+         name == "TMAX") %>%
+  mutate(max = max - 60,
+         min = min - 60,
+         x5 = x5 - 60,
+         x20 = x20 - 60,
+         x40 = x40 - 60,
+         x60 = x60 - 60,
+         x80 = x80 - 60,
+         x95 = x95 - 60)
+
+legend.labels <- legend.df %>%
+  pivot_longer(cols = c(max, min, starts_with("x")),
+               names_to = "levels") %>%
+  mutate(label = case_when(
+    levels == "max" ~ "max",
+    levels == "min" ~ "min",
+    levels == "x95" ~ "95th percentile",
+    TRUE ~ paste0(str_sub(levels, 2, -1), "th")
+  )) %>%
+  mutate(filter_day = ifelse(
+    levels %in% c("max", "x80", "x40", "x5"),
+    min(day_of_year),
+    max(day_of_year)
+  )) %>%
+  filter(day_of_year == filter_day)
+
+##  Add legend
+max.graph2 <- max.graph +
+  # ribbon between the lowest and 5th percentiles
+  geom_ribbon(data = legend.df,
+              aes(ymin = min, ymax = x5),
+              fill = "#bdc9e1") +
+  # ribbon between the 5th and 20th percentiles
+  geom_ribbon(data = legend.df,
+              aes(ymin = x5, ymax = x20),
+              fill = "#74a9cf") +
+  # ribbon between the 20th and 40th percentiles
+  geom_ribbon(data = legend.df,
+              aes(ymin = x20, ymax = x40),
+              fill = "#2b8cbe") +
+  # ribbon between the 40th and 60th percentiles
+  geom_ribbon(data = legend.df,
+              aes(ymin = x40, ymax = x60),
+              fill = "#045a8d") +
+  # ribbon between the 60th and 80th percentiles
+  geom_ribbon(data = legend.df,
+              aes(ymin = x60, ymax = x80),
+              fill = "#2b8cbe") +
+  # ribbon between the 80th and 95th percentiles
+  geom_ribbon(data = legend.df,
+              aes(ymin = x80, ymax = x95),
+              fill = "#74a9cf") +
+  # ribbon between the 95th percentile and the max
+  geom_ribbon(data = legend.df,
+              aes(ymin = x95, ymax = max),
+              fill = "#bdc9e1") +
+  ggrepel::geom_text_repel(data = filter(legend.labels,
+                                         filter_day == max(filter_day)),
+                           aes(y = value, label = label),
+                           min.segment.length = 0, size = 3,
+                           direction = "y", hjust = 0, nudge_x = 5) +
+  ggrepel::geom_text_repel(data = filter(legend.labels,
+                                         filter_day == min(filter_day)),
+                           aes(y = value, label = label),
+                           min.segment.length = 0, size = 3,
+                           direction = "y", hjust = 1, nudge_x = -5)
+
+ggsave("graphs/DailyHighTemp_USW00014839.png", plot = max.graph2,
+       width = 8)
+
+
