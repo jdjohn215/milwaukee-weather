3896332402e9383169668d67548e840d8cdd1e93
jdjohn215 <jdjohn215@gmail.com>
2022-02-19 14:33:42 -0600

    init

---
+++ R/Retrieve_GHCN_USW00014839.R
@@ -0,0 +1,40 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+
+
+# station readme
+#   https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/readme-by_station.txt
+
+# download the zipped file
+temp <- tempfile()
+download.file("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_station/USW00014839.csv.gz",temp)
+
+# unzip and read
+ghcn <- read_csv(temp,
+                 col_names = c("id", "yearmoda", "element", "value",
+                               "mflag", "qflag", "sflag", "obs_time"),
+                 col_types = "cccncccc")
+
+# delete the zipped file
+unlink(temp)
+
+# subset and format
+ghcn.wide <- ghcn %>%
+  select(yearmoda, element, value) %>%
+  filter(element %in% c("PRCP", "SNOW", "SNWD", "TMAX", "TMIN")) %>%
+  separate(col = yearmoda, sep = c(4,6), into = c("year", "month", "day")) %>%
+  pivot_wider(names_from = element, values_from = value) %>%
+  # convert from tenths of mm to inches
+  mutate(PRCP = PRCP * 0.00393701,
+         SNOW = SNOW * 0.00393701,
+         SNWD = SNWD * 0.00393701) %>%
+  # convert from tenths of degrees C to F
+  mutate(TMAX = ((TMAX / 10) * (9/5)) + 32,
+         TMIN = ((TMIN / 10) * (9/5)) + 32) %>%
+  mutate(date = as.Date(paste(year, month, day, sep = "-")),
+         day_of_year = lubridate::yday(date)) %>%
+  select(year, month, day, date, day_of_year, PRCP, SNOW, SNWD,
+         TMAX, TMIN)
+
+write_csv(ghcn.wide, "data/GHCN_USW00014839.csv")
---
+++ data/README.md
@@ -0,0 +1,15 @@
+## Milwaukee daily weather history bulk download
+
+This file (`data/GHCN_USW00014839.csv`) contains daily weather reports from Milwaukee Mitchell Airport (GHCND:USW00014839). The start date is April 1, 1938.
+
+The data fields are:
+
+* PRCP - precipitation (inches)
+* SNOW - snowfall (inches)
+* SNWD - snow depth (inches)
+* TMAX - maximum temperature (degrees Fahrenheit)
+* TMIN - minimum temperature
+
+Detailed field definitions are in [this NOAA documentation file](ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt).
+
+This file is created by running `R/Retrieve_GHCN_USW00014839.R`. That script downloads the latest data, unzips it, filters for the desired values, converts those values to the desired units, formats date columns, and converts the data from long to wide format.
