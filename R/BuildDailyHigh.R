e6961f49d74e4b98ede697f64811d177d4681963
jdjohn215 <jdjohn215@gmail.com>
2022-02-20 19:36:29 -0600

    improve graph legend and labels

--- R/BuildDailyHigh.R
+++ R/BuildDailyHigh.R
@@ -54,33 +54,24 @@ max.graph <- daily.summary.stats %>%
   # draw vertical lines for the months
   geom_vline(xintercept = c(month.breaks$day_of_year, 365),
              linetype = "dotted", lwd = 0.2) +
-  # ribbon between the lowest and 5th percentiles
-  geom_ribbon(aes(ymin = min, ymax = x5),
+  # ribbon between the lowest and 5th, 95th and max percentiles
+  geom_ribbon(aes(ymin = min, ymax = max),
               fill = "#bdc9e1") +
-  # ribbon between the 5th and 20th percentiles
-  geom_ribbon(aes(ymin = x5, ymax = x20),
+  # ribbon between the 5th and 20th, 80th to 95th percentiles
+  geom_ribbon(aes(ymin = x5, ymax = x95),
               fill = "#74a9cf") +
-  # ribbon between the 20th and 40th percentiles
-  geom_ribbon(aes(ymin = x20, ymax = x40),
+  # ribbon between the 20th and 40th, 60th and 80th percentiles
+  geom_ribbon(aes(ymin = x20, ymax = x80),
               fill = "#2b8cbe") +
   # ribbon between the 40th and 60th percentiles
   geom_ribbon(aes(ymin = x40, ymax = x60),
               fill = "#045a8d") +
-  # ribbon between the 60th and 80th percentiles
-  geom_ribbon(aes(ymin = x60, ymax = x80),
-              fill = "#2b8cbe") +
-  # ribbon between the 80th and 95th percentiles
-  geom_ribbon(aes(ymin = x80, ymax = x95),
-              fill = "#74a9cf") +
-  # ribbon between the 95th percentile and the max
-  geom_ribbon(aes(ymin = x95, ymax = max),
-              fill = "#bdc9e1") +
   # y-axis breaks
   geom_hline(yintercept = seq(-10, 100, 10),
              color = "white", lwd = 0.1) +
   # line for this year's values
   geom_line(data = this.year,
-            aes(y = TMAX)) +
+            aes(y = TMAX), lwd = 1) +
   # points for maximum records set this year
   geom_point(data = filter(record.status.this.year, 
                            name == "TMAX",
@@ -101,8 +92,13 @@ max.graph <- daily.summary.stats %>%
                      labels = month.breaks$month_name,
                      name = NULL) +
   labs(title = "Daily High Temperature at Milwaukee's Mitchell Airport",
-       subtitle = paste("Daily weather summaries from April 1, 1938 through",
-                        format(last.date, "%B %d, %Y"))) +
+       subtitle = paste("The line shows daily highs for",
+                        paste0(lubridate::year(last.date), "."),
+                        "The ribbons cover the",
+                        "historical range. The last date shown is", 
+                        format(last.date, "%b %d, %Y.")),
+       caption = paste("Records begin on April 1, 1938.",
+                       "This graph was last updated on", format(Sys.Date(), "%B %d, %Y"))) +
   theme(panel.background = element_blank(),
         panel.border = element_blank(),
         panel.grid = element_blank(),
@@ -125,13 +121,32 @@ legend.df <- daily.summary.stats %>%
          x80 = x80 - 60,
          x95 = x95 - 60)
 
+legend.line.df <- tibble(
+  day_of_year = 165:201,
+  temp = case_when(
+    day_of_year == 165 ~ legend.df$x40[legend.df$day_of_year == 165],
+    day_of_year == 168 ~ legend.df$x40[legend.df$day_of_year == 165] + 3,
+    day_of_year == 172 ~ legend.df$x40[legend.df$day_of_year == 165] - 4,
+    day_of_year == 177 ~ legend.df$min[legend.df$day_of_year == 177] - 1,
+    day_of_year == 180 ~ legend.df$x20[legend.df$day_of_year == 180] - 1,
+    day_of_year == 182 ~ legend.df$x60[legend.df$day_of_year == 182] + 1,
+    day_of_year == 185 ~ legend.df$x60[legend.df$day_of_year == 185] - 6,
+    day_of_year == 189 ~ legend.df$max[legend.df$day_of_year == 189] + 1,
+    day_of_year == 194 ~ legend.df$x60[legend.df$day_of_year == 194],
+    day_of_year == 198 ~ legend.df$x40[legend.df$day_of_year == 198],
+    day_of_year == 201 ~ legend.df$x60[legend.df$day_of_year == 201],
+    TRUE ~ NA_real_
+  )
+) %>%
+  filter(!is.na(temp))
+
 legend.labels <- legend.df %>%
   pivot_longer(cols = c(max, min, starts_with("x")),
                names_to = "levels") %>%
   mutate(label = case_when(
     levels == "max" ~ "max",
     levels == "min" ~ "min",
-    levels == "x95" ~ "95th percentile",
+    levels == "x95" ~ "95th percentile of past years",
     TRUE ~ paste0(str_sub(levels, 2, -1), "th")
   )) %>%
   mutate(filter_day = ifelse(
@@ -145,32 +160,31 @@ legend.labels <- legend.df %>%
 max.graph2 <- max.graph +
   # ribbon between the lowest and 5th percentiles
   geom_ribbon(data = legend.df,
-              aes(ymin = min, ymax = x5),
+              aes(ymin = min, ymax = max),
               fill = "#bdc9e1") +
   # ribbon between the 5th and 20th percentiles
   geom_ribbon(data = legend.df,
-              aes(ymin = x5, ymax = x20),
+              aes(ymin = x5, ymax = x95),
               fill = "#74a9cf") +
   # ribbon between the 20th and 40th percentiles
   geom_ribbon(data = legend.df,
-              aes(ymin = x20, ymax = x40),
+              aes(ymin = x20, ymax = x80),
               fill = "#2b8cbe") +
   # ribbon between the 40th and 60th percentiles
   geom_ribbon(data = legend.df,
               aes(ymin = x40, ymax = x60),
               fill = "#045a8d") +
-  # ribbon between the 60th and 80th percentiles
-  geom_ribbon(data = legend.df,
-              aes(ymin = x60, ymax = x80),
-              fill = "#2b8cbe") +
-  # ribbon between the 80th and 95th percentiles
-  geom_ribbon(data = legend.df,
-              aes(ymin = x80, ymax = x95),
-              fill = "#74a9cf") +
-  # ribbon between the 95th percentile and the max
-  geom_ribbon(data = legend.df,
-              aes(ymin = x95, ymax = max),
-              fill = "#bdc9e1") +
+  geom_line(data = legend.line.df, aes(y = temp), lwd = 0.9) +
+  geom_point(aes(x = 177, y = legend.line.df$temp[legend.line.df$day_of_year == 177]),
+             color = "blue") +
+  geom_point(aes(x = 189, y = legend.line.df$temp[legend.line.df$day_of_year == 189]),
+             color = "red") +
+  geom_text(aes(x = 180, y = legend.line.df$temp[legend.line.df$day_of_year == 177] - 2,
+                label = "record low this year"),
+            hjust = 0, size = 3) +
+  geom_text(aes(x = 192, y = legend.line.df$temp[legend.line.df$day_of_year == 189] + 2,
+                label = "record high this year"),
+            hjust = 0, size = 3) +
   ggrepel::geom_text_repel(data = filter(legend.labels,
                                          filter_day == max(filter_day)),
                            aes(y = value, label = label),
